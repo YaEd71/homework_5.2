5c7f08e8a7222c05991b104820c6d3689df34eae
YaEd71 <166940263+YaEd71@users.noreply.github.com>
2024-05-26 20:15:03 +0300

    Add files via upload

---
+++ homework_5.2_spec.c.m..py
@@ -0,0 +1,17 @@
+class House:
+    def __init__(self, numberOfFoors):
+        self.numberOfFoors = numberOfFoors
+
+
+    def __set__(self, NewNumberOfFloorsfloors):
+        set.NewNumberOfFloors = floors
+        print(floors)
+
+numberOfFoors = 0
+floors = 25
+
+print(numberOfFoors)
+print(floors)
+
+numberOfFoors = House
+NewNumberOfFloors = House
